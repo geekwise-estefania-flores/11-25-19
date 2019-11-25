8611dd00fd9207f5d40a1d56fbb38c24b7d7ccee
geekwise-estefania-flores <geekwise.estefania.flores@gmail.com>
2019-11-25 23:02:29 +0000

    setup foreignkey for account and product for customer

--- app/drf/bank/customer.py
+++ app/drf/bank/customer.py
@@ -1,6 +1,19 @@
 from django.db import models
+from drf.product import Product
+from drf.account import Account
 
 class Customer(models.Model):
     customer_first_name = models.CharField(max_length = 300)
     customer_last_name = models.CharField(max_length = 300)
     customer_phone_number = models.CharField(max_length = 15)
+
+    product = models.ForeignKey('auth.User', related_name='product', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product}'
+
+    account = models.ForeignKey('auth.User', related_name='account', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.account}'
+
